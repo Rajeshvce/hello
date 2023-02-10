1c5cf489d0805ce40445eb78e94454d56cd2815e
Rajeshvce <94607772+Rajeshvce@users.noreply.github.com>
2023-02-10 12:27:32 +0530

    Add files via upload

---
+++ hello_new/hello.c
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2016 ilbers GmbH
+ *
+ * This program is licensed under the terms of the GNU General Public License
+ * version 2.
+ */
+
+#include <hello.h>
+
+int main(void)
+{
+	say_hello();
+        printf("This is my first recipe\n");
+	return 0;
+}
